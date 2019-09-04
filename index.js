536d666dc21784ff6f4753f6f91fcd016f0e0d69
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-09-04 10:01:36 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+let dataset = [80, 100, 56, 36, 77, 12, 14];
+
+let svgWidth = 500, svgHeight = 300, barPadding = 5;
+let barWidth = (svgWidth / dataset.length);
+
+let svg = d3.select("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+
+let barChart = svg.selectAll("rect")
+    .data(dataset)
+    .enter()
+    .append("rect")
+    .attr("y", function(d) {
+        return svgHeight - d
+    })
+    .attr("width", barWidth - barPadding)
+    .attr("transform", function (d, i) {
+        let translate = [barWidth * i, 0];
+        return "translate(" + translate + ")";
+    });
