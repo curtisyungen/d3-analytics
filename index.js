eab1a253e85ece013947595a21b8497cfb2d4b93
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-09-04 12:50:52 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
-let dataset = [80, 100, 56, 36, 77, 12, 14];
+let dataset = [1, 2, 3, 4, 5];
 
 let svgWidth = 500, svgHeight = 300, barPadding = 5;
-let barWidth = (svgWidth / dataset.length);
+let barWidth = 20; //(svgWidth / dataset.length);
+let leftPadding = 30;
+let bottomPadding = 20;
 
 // Set dimensions for chart
 let svg = d3.select("svg")
     .attr("width", svgWidth)
-    .attr("height", svgHeight);
+    .attr("height", svgHeight + 20);
+
+// Add horizontal scale to chart
+let xScale = d3.scaleLinear()
+    .domain([0, d3.max(dataset)])
+    .range([0, svgWidth]);
 
 // Add vertical scale to chart
 let yScale = d3.scaleLinear()
     .domain([0, d3.max(dataset)])
-    .range([0, svgHeight]);
+    .range([svgHeight, 0]);
+
+// Add x axis to chart
+let x_axis = d3.axisBottom()
+    .scale(xScale);
+
+let xAxisTranslate = svgHeight - bottomPadding;
+
+svg.append("g")
+    .attr("transform", `translate(${leftPadding}, ${xAxisTranslate})`)
+    .call(x_axis);
+
+// Add y axis to chart
+let y_axis = d3.axisLeft()
+    .scale(yScale);
+
+svg.append("g")
+    .attr("transform", `translate(${leftPadding}, ${bottomPadding - 40})`)
+    .call(y_axis);
 
 // Add data to bar chart
 let barChart = svg.selectAll("rect")
@@ -19,14 +44,14 @@ let barChart = svg.selectAll("rect")
     .enter()
     .append("rect")
     .attr("y", function(d) {
-        return svgHeight - yScale(d)
+        return yScale(d)
     })
     .attr("height", function(d) {
-        return yScale(d)
+        return svgHeight - yScale(d) - bottomPadding
     })
     .attr("width", barWidth - barPadding)
     .attr("transform", function (d, i) {
-        let translate = [barWidth * i, 0];
+        let translate = [barWidth * i + leftPadding, 0];
         return "translate(" + translate + ")";
     });
 
