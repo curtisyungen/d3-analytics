e5d4467ccb69a7793c8d3b6f2f0f29b87a59dcc7
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-09-04 10:16:27 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ let dataset = [80, 100, 56, 36, 77, 12, 14];
 let svgWidth = 500, svgHeight = 300, barPadding = 5;
 let barWidth = (svgWidth / dataset.length);
 
+// Set dimensions for chart
 let svg = d3.select("svg")
     .attr("width", svgWidth)
     .attr("height", svgHeight);
 
+// Add data to bar chart
 let barChart = svg.selectAll("rect")
     .data(dataset)
     .enter()
@@ -22,3 +24,19 @@ let barChart = svg.selectAll("rect")
         let translate = [barWidth * i, 0];
         return "translate(" + translate + ")";
     });
+
+// Add data labels to bar chart
+let text = svg.selectAll("text")
+    .data(dataset)
+    .enter()
+    .append("text")
+    .text(function(d) {
+        return d;
+    })
+    .attr("y", function(d, i) {
+        return svgHeight - d - 2;
+    })
+    .attr("x", function(d, i) {
+        return barWidth * i;
+    })
+    .attr("fill", "blue");
